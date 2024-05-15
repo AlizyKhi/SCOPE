8822939024b9dd785763f7cd195e8ce691d09990
AlizyKhi <162714208+AlizyKhi@users.noreply.github.com>
2024-05-15 22:07:49 +0500

    Add files via upload
    Begining Of SCOPE Practice

---
+++ index.ts
@@ -0,0 +1,52 @@
+var a=10;
+function number(){
+    var a=100;
+    if (true){
+        var a=200;
+    }
+    console.log(a)
+}
+number();
+
+var z=10;
+function nnumber(){
+    var z=100;
+    if (true){
+        var z=1000;
+    }
+    console.log(z)
+}
+nnumber();
+
+let math=true;
+function boolean(){
+    let y=false;
+    if (false){
+        let y=true;
+    }
+    console.log(y)
+}
+boolean();
+
+var fullName=1000;
+function plus(){
+    var fullName=1000;
+    if (true){
+        var fullName=500;
+    }
+    console.log(fullName)
+}
+plus();
+
+let bics=true;
+function identity(){
+    let a=false;
+    if (false){
+        let z=false;
+    }
+    console.log(a)
+}
+identity();
+
+
+
